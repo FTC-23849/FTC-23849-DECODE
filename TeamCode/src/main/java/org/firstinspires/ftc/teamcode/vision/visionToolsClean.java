9ce688a203ec2f637f5d84fb898fdc54664ef247
ashviz915 <avyukth.sunil@gmail.com>
2026-04-25 16:26:05 -0700

    power change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/visionToolsClean.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/visionToolsClean.java
@@ -422,12 +422,12 @@ public class visionToolsClean {
         if(currentDist < 1.9) {
             speed = 31.7460 * x2
                     - 428.254 * x
-                    - 209.4921;
+                    - 189.4921;
 
         }else if(currentDist< 2.75){
             speed = 58.0514 * x2
                     - 491.1754 * x
-                    - 167.2411;
+                    - 137.2411;
         }else{
             //−19.9936x3+191.7677x2−853.9049x−213.5501
 
@@ -435,7 +435,7 @@ public class visionToolsClean {
                     + 528.9893 * x3
                     - 3290.8152 * x2
                     + 8681.1264 * x
-                    - 9164.4227;
+                    - 9184.4227;
 
         }
         return speed;
