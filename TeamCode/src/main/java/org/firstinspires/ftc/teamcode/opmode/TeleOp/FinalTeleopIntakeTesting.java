47d4e253d86128d1a8b30010fef00013a2f56b59
ashviz915 <avyukth.sunil@gmail.com>
2026-04-26 15:32:06 -0700

    fixed turret offsets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/FinalTeleopIntakeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/FinalTeleopIntakeTesting.java
@@ -96,8 +96,8 @@ public class FinalTeleopIntakeTesting extends OpMode {
     public static double red = 0;
     public static double blue = 0;
     //more for left, less for right
-    public static double turretZeroCorrection = -0.006;
-    public static double turretZeroCorrection2 = -0.01;
+    public static double turretZeroCorrection = -0.003;
+    public static double turretZeroCorrection2 = -0.008;
     //    public static double VKp = 0.02;
 //    public static double VKi = 0.003;
 //    public static double VKd = 0;
@@ -129,7 +129,7 @@ public class FinalTeleopIntakeTesting extends OpMode {
     boolean firstLoop = true;
     String allianceColor = "Red";
     boolean recycleIntakeTimerStarted = false;
-    public static double turretCorrection = 0.004;
+    public static double turretCorrection = 0.003;
     boolean shooting;
     boolean yPressed = false;
     boolean purpleSortingEnabled = false;
